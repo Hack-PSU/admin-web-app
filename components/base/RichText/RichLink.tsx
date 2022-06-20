4842c1b2645e02c129a099a5a36c3635ffb8cf2c
susanto-tm <susanto.tm@gmail.com>
2022-06-20 02:19:10 -0400

    Fixed a tag when decorating editor.

--- components/base/RichText/RichLink.tsx
+++ components/base/RichText/RichLink.tsx
@@ -1,11 +1,15 @@
-import { FC } from "react";
-import { Link } from "@mui/material";
+import React, { FC } from "react";
 
 // Get props type from https://github.com/facebook/draft-js/blob/main/src/model/decorators/DraftDecorator.js
 const RichLink: FC = (props: any) => {
   const url = props.contentState.getEntity(props.entityKey).getData().url;
   return (
-    <a href={url} style={{ textDecoration: "underline", color: "blue" }}>
+    <a
+      href={url}
+      style={{ textDecoration: "underline", color: "blue" }}
+      rel="noreferrer noopener"
+      target="_blank"
+    >
       {props.children}
     </a>
   );
